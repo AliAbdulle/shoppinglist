fd062ad5c4e3402258c55324555b2c6a992be9a9
AliAbdulle <alidhobey@gmail.com>
2020-05-30 18:55:52 -0500

    Service list page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react"
+import Header from "./components/Header"
+import Home from "./Pages/Home"
+import ServicesList from "./Pages/services/ServicesList"
+import ServiceDetail from "./Pages/services/ServiceDetail"
+
+import {Switch, Route} from "react-router-dom"
 
 function App() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/services" >
+                    <ServicesList />
+                </Route>
+                <Route path="/services" >
+                    <ServiceDetail />
+                </Route>
+            </Switch>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Pages/services/ServicesList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function ServicesList() {
+    return (
+        <h1> Service List Page</h1>
+    )
+}
+export default ServicesList
