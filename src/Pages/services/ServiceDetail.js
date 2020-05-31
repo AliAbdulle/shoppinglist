8ec397108904486672f2d631b38932937fd6c232
AliAbdulle <alidhobey@gmail.com>
2020-05-31 18:52:01 -0500

    Sarvice data restructers

--- src/Pages/services/ServiceDetail.js
+++ src/Pages/services/ServiceDetail.js
@@ -1,9 +1,18 @@
-import React from "react"
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import servicesData from "./servicesData";
 function ServiceDetail(props) {
-    return (
-        <h1>Service Detail Page</h1>
-    )
+    const {serviceId} = useParams()
+    const thisService = servicesData.find(service => service.Id === serviceId)
+  return (
+    <div>
+      <h1>Service Detail Page</h1>
+      <h3>
+        {thisService.name} -${thisService.price}
+      </h3>
+      <p>{thisService.description}</p>
+    </div>
+  );
 }
 
-export default ServiceDetail
+export default ServiceDetail;
