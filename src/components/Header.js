6630d653eb706186383bac300874d3d494329046
AliAbdulle <alidhobey@gmail.com>
2020-05-31 18:25:34 -0500

    saved the heades section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react"
+import {Link} from "react-router-dom"
 
-function Header () {
+function Header() {
     return (
-        <Header>
+        <header>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/services">Services</Link></li>
             </ul>
-        </Header>
+        </header>
     )
 }
+
 export default Header
